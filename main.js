ce9743278692dba52718a47b10d6a9a803639f08
Fran Ruiz <franciscxruiz@gmail.com>
2024-03-21 16:05:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+(function () {
+  document.querySelector(".toogle-menu").addEventListener("click", function () {
+    document.querySelector(".main-container").classList.toggle("abrir");
+  });
+})();
